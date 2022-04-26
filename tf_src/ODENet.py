faad43e847901fbc5dcc048c834c0785df019349
ScSteffen <steffen.schotthoefer@outlook.de>
2022-04-26 23:02:03 +0200

    started time series training

--- tf_src/ODENet.py
+++ tf_src/ODENet.py
@@ -4,69 +4,132 @@ import numpy as np
 from tensorflow import keras
 
 
-class LinODENet(keras.Model):
+# ---- Network definitions ---
 
-    def __init__(self, input_dim=1, output_dim=1, name="partDLRANet", **kwargs):
-        super(LinODENet, self).__init__(name=name, **kwargs)
-        self.odeBlock = LinearOdeBlock(input_dim=input_dim)
+class ODENet(keras.Model):
+    # virtual base class
+
+    odeBlock: tf.Tensor
+
+    def __init__(self, input_dim=1, name="NonLinODENet", **kwargs):
+        super(ODENet, self).__init__(name=name, **kwargs)
 
     def call(self, inputs, t_0, t_f):
-        z = self.odeBlock(inputs=inputs,t_0=t_0,t_f=t_f)
+        z = self.odeBlock(inputs=inputs, t_0=t_0, t_f=t_f)
         return z
 
-class NonLinODENet(keras.Model):
+    def predict(self, inputs, t_0, t_f, n_time):
+        z = self.odeBlock.predict(
+            inputs=inputs, t_0=t_0, t_f=t_f, n_time=n_time)
+        return z
 
-    def __init__(self, input_dim=1, output_dim=1, name="partDLRANet", **kwargs):
+    def save(self, folder_name):
+        self.odeBlock.save(folder_name)
+        return 0
+
+    def load(self, folder_name):
+        self.odeBlock.load(folder_name)
+        return 0
+
+
+class NonLinODENet(ODENet):
+
+    def __init__(self, input_dim=1,  name="NonLinODENet", **kwargs):
         super(LinODENet, self).__init__(name=name, **kwargs)
         self.odeBlock = NonLinearOdeBlock(input_dim=input_dim)
 
-    def call(self, inputs, t_0, t_f):
-        z = self.odeBlock(inputs=inputs,t_0=t_0,t_f=t_f)
-        return z
 
-class LinearOdeBlock(keras.layers.Layer):
-    # self.layer1 = Linear(units=units)
+class LinODENet(ODENet):
 
-    def __init__(self, input_dim=32):
-        super(LinearOdeBlock, self).__init__()
+    def __init__(self, input_dim=1, name="LinODENet", **kwargs):
+        super(LinODENet, self).__init__(name=name, **kwargs)
+        self.odeBlock = LinearOdeBlock(input_dim=input_dim)
+
+# ---- Layer definitions ----
+
+
+class ODEBlock(keras.layers.Layer):
+    A: tf.Tensor
+    b: tf.Tensor
+    # virtual base class
+
+    def __init__(self, input_dim=2):
+        super(ODEBlock, self).__init__()
         # Right now, only quadratic matrices
-        self.A = self.add_weight(shape=(input_dim, input_dim), initializer="random_normal", name="_A", trainable=True)
-        self.b = self.add_weight(shape=(input_dim,), initializer="zeros", name="_b", trainable=True)
+        self.A = self.add_weight(shape=(
+            input_dim, input_dim), initializer="random_normal", name="_A", trainable=True)
+        self.b = self.add_weight(
+            shape=(input_dim,), initializer="zeros", name="_b", trainable=True)
 
     def call(self, inputs, t_0, t_f):
         z = tfp.math.ode.BDF().solve(self.ode_fn, t_0, inputs, solution_times=[t_f],
                                      constants={'A': self.A, 'b': self.b})
         return z
+
+    def predict(self, inputs, t_0, t_f, n_time):
+        times = np.linspace(t_0, t_f, n_time).tolist()
+
+        z = tfp.math.ode.BDF().solve(self.ode_fn, t_0, inputs, solution_times=times,
+                                     constants={'A': self.A, 'b': self.b})
+        return z
+
+    def save(self, folder_name):
+
+        a_np = self.A.numpy()
+        # print(a_np)
+        np.save(folder_name+"/A.npy", a_np)
+
+        b_np = self.b.numpy()
+        np.save(folder_name+"/b.npy", b_np)
+
+        return 0
+
+    def load(self, folder_name):
+        a_np = np.load(folder_name+"/A.npy")
+        self.A = tf.Variable(initial_value=a_np,
+                             trainable=True, name="_A", dtype=tf.float32)
+        b_np = np.load(folder_name+"/b.npy")
+        self.b = tf.Variable(initial_value=b_np,
+                             trainable=True, name="_b", dtype=tf.float32)
+        return 0
+
+
+class LinearOdeBlock(ODEBlock):
+
+    def __init__(self, input_dim=2):
+        super(LinearOdeBlock, self).__init__(input_dim=input_dim)
 
     def ode_fn(self, t, y, A, b):
         # Right Hand side of the Ode that defines the network
         return tf.linalg.matvec(A, y) + b
-    
-class NonLinearOdeBlock(keras.layers.Layer):
-    # self.layer1 = Linear(units=units)
 
-    def __init__(self, input_dim=32):
-        super(NonLinearOdeBlock, self).__init__()
-        # Right now, only quadratic matrices
-        self.A = self.add_weight(shape=(input_dim, input_dim), initializer="random_normal", name="_A", trainable=True)
-        self.b = self.add_weight(shape=(input_dim,), initializer="zeros", name="_b", trainable=True)
 
-    def call(self, inputs, t_0, t_f):
-        z = tfp.math.ode.BDF().solve(self.ode_fn, t_0, inputs, solution_times=[t_f],
-                                     constants={'A': self.A, 'b': self.b})
-        return z
+class NonLinearOdeBlock(ODEBlock):
+
+    def __init__(self, input_dim=2):
+        super(NonLinearOdeBlock, self).__init__(input_dim=input_dim)
 
     def ode_fn(self, t, y, A, b):
         # Right Hand side of the Ode that defines the network
         return tf.keras.activations.relu(tf.linalg.matvec(A, y) + b)
 
 
-def create_lotka_volterra_data(n_data: int, n_time: int, final_time: float) -> np.ndarray:
+class NonLinearOdeBlockV2(ODEBlock):
+
+    def __init__(self, input_dim=2):
+        super(NonLinearOdeBlockV2, self).__init__(input_dim=input_dim)
+
+    def ode_fn(self, t, y, A, b):
+        # Right Hand side of the Ode that defines the network
+        return tf.linalg.matvec(A, tf.keras.activations.relu(y)) + b
+
+
+def create_lotka_volterra_data(n_data: int, n_time: int, final_time: float, deterministic=True) -> np.ndarray:
     """
     :param n_data: number of different initial conditions
            n_time: number of time steps of the solution
            final_time: final time of computed system
-    :return: time series data of the systems dynamics: dims=(2 x n_time x n_data)
+    :return: time series data of the systems dynamics: dims=(n_data x n_time x 2)
     """
     a = 2.0 / 3.0
     b = 4.0 / 3.0
@@ -81,18 +144,23 @@ def create_lotka_volterra_data(n_data: int, n_time: int, final_time: float) -> n
         times = [t_f]
 
     def lotka_volterra_ode(t, x):
-        res = np.asarray([a * x[0] - b * x[0] * x[1], c * x[0] * x[1] - d * x[1]])
+        res = np.asarray([a * x[0] - b * x[0] * x[1],
+                         c * x[0] * x[1] - d * x[1]])
         z = tf.constant(res, dtype=x.dtype, shape=x.shape)
         return z
 
     data = np.zeros(shape=(n_data, n_time, 2))
-    tf.random.set_seed(5);
+    tf.random.set_seed(5)
 
     for i_data in range(n_data):
-        x_0 = tf.random.uniform(shape=[2, ], minval=0.5, maxval=5, dtype=tf.float64, seed=1)
-        # x_0 = tf.constant([1., 1.], dtype=tf.float64)
+        if deterministic:
+            x_0 = tf.constant([1., 1.], dtype=tf.float64)
+        else:
+            x_0 = tf.random.uniform(
+                shape=[2, ], minval=0.5, maxval=5, dtype=tf.float64, seed=1)
 
-        results = tfp.math.ode.BDF().solve(lotka_volterra_ode, t_0, x_0, solution_times=times)
+        results = tfp.math.ode.BDF().solve(
+            lotka_volterra_ode, t_0, x_0, solution_times=times)
         data[i_data, :, :] = results.states
 
     return data
@@ -105,10 +173,6 @@ def create_pendulum_data(n_data: int, n_time: int, final_time: float) -> np.ndar
            final_time: final time of computed system
     :return: time series data of the systems dynamics: dims=(2 x n_time x n_data)
     """
-    a = 2.0 / 3.0
-    b = 4.0 / 3.0
-    c = 1.0
-    d = 1.0
 
     t_0 = 0.0
     t_f = final_time
@@ -117,20 +181,21 @@ def create_pendulum_data(n_data: int, n_time: int, final_time: float) -> np.ndar
     if n_time == 1:
         times = [t_f]
 
-    A = tf.constant([[-1., -2.], [-3., -4.]], dtype=tf.float64)
+    A = tf.constant([[0., -1.], [-1., 0.]], dtype=tf.float64)
 
-    def oscillator_ode(t, x):
+    @tf.function
+    def linear_ode(t, x):
         return tf.linalg.matvec(A, x)
 
-
     data = np.zeros(shape=(n_data, n_time, 2))
-    tf.random.set_seed(5);
+    tf.random.set_seed(5)
 
     for i_data in range(n_data):
-        x_0 = tf.random.uniform(shape=[2, ], minval=0.5, maxval=5, dtype=tf.float64, seed=1)
+        x_0 = tf.random.uniform(
+            shape=[2, ], minval=0.5, maxval=5, dtype=tf.float64, seed=1)
         # x_0 = tf.constant([1., 1.], dtype=tf.float64)
 
-        results = tfp.math.ode.BDF().solve(lotka_volterra_ode, t_0, x_0, solution_times=times)
+        results = tfp.math.ode.BDF().solve(linear_ode, t_0, x_0, solution_times=times)
         data[i_data, :, :] = results.states
 
     return data
--- tf_src/neural_ode_implementation.py
+++ tf_src/neural_ode_implementation.py
@@ -9,46 +9,72 @@ import tensorflow_probability as tfp
 import numpy as np
 import matplotlib.pyplot as plt
 
-from ODENet import LinODENet, create_lotka_volterra_data
+from ODENet import LinODENet, NonLinODENet, create_lotka_volterra_data, create_pendulum_data
+
 
 def main():
-    train_linear_ODENet()
-    
-    #train_nonlinear_ODENet()
+
+    train = True
+    test = False
+    # Create Model
+    model = NonLinODENet(input_dim=2)
+    #model = NonLinearOdeNet(input_dim=2)
+
+    if train:
+        # Load Model
+        save_name = 'linearODENet/lotka_volterra'
+        model.load(save_name)
+
+        # Create data from a rea dynamical system
+        n_data = 1
+        n_time = 20
+        final_time = 10
+        data = create_lotka_volterra_data(
+            n_data=n_data, n_time=n_time, final_time=final_time, deterministic=True)
+
+        #data = create_pendulum_data(
+        #    n_data=n_data, n_time=n_time, final_time=final_time)
+
+        # Train Model
+        training_loop(model, data, final_time, n_time,save_name)
+
+        # Test Model
+        test_model(
+            model, ic=data[0, 0, :], time_data=data[0, :, :], t_f=final_time, n_time=n_time)
+
+    if test:
+        # Load Model
+        model.load('linearODENet/lotka_volterra')
+        # Create data from a rea dynamical system
+        n_data = 1
+        n_time = 200
+        final_time = 5
+        data = create_lotka_volterra_data(
+            n_data=n_data, n_time=n_time, final_time=final_time,deterministic=True)
+        # Evaluate model
+        test_model(
+            model, ic=data[0, 0, :], time_data=data[0, :, :], t_f=final_time, n_time=n_time)
+    # train_nonlinear_ODENet()
     return 0
 
-def train_linear_ODENet():
-    model = LinODENet(input_dim=2)  # Build Model
+
+def training_loop(model, data, t_f, n_time, save_name):
+
+    # config training
+    epochs = 1
+    batch = 1
+    t_0 = 0.0
+    dt = t_f/float(n_time)
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
     mse_loss_fn = tf.keras.losses.MeanSquaredError()
 
     loss_metric = tf.keras.metrics.Mean()
 
-    # Create data from a rea dynamical system
-    n_data = 10
-    n_time = 100
-    final_time = 20
-    data = create_lotka_volterra_data(n_data=n_data, n_time=n_time, final_time=final_time)
-    plt.plot(np.linspace(0, final_time, n_time), data[0, :, :])
-    plt.show()
-
-    # Build dataset
-    split = int(n_data / 10)
-    train_x = data[:n_data - split, 0, :]
-    train_y = data[:n_data - split, -1, :]
-
-    test_x = data[- split:, -1, :]
-    test_y = data[- split:, -1, :]
-
-    # config training
-    epochs = 25
-    batch = 1
-    t_0 = 0.0
-    t_f = final_time
+    train_x = data[0, :-1, :]
+    train_y = data[0, 1:, :]  # label is the solution at the next time step
 
     train_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
-    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch)
 
     # Iterate over epochs.
     for epoch in range(epochs):
@@ -58,8 +84,9 @@ def train_linear_ODENet():
         for step, batch_train in enumerate(train_dataset):
             with tf.GradientTape() as tape:
                 # tape.watch(modA)
-                results = model(inputs=batch_train[0], t_0=t_0, t_f=t_f)
-                x_f = results.states[0] 
+                results = model(
+                    inputs=batch_train[0], t_0=step*dt, t_f=(step+1)*dt)
+                x_f = results.states[0]
                 # Compute reconstruction loss
                 loss = mse_loss_fn(batch_train[1], x_f)
                 loss += sum(model.losses)  # Add KLD regularization loss
@@ -69,13 +96,34 @@ def train_linear_ODENet():
 
             loss_metric(loss)
 
-            if step % 100 == 0:
-                print("step %d: mean loss = %.4f" % (step, loss_metric.result()))
+            if step % 10 == 0:
+                print("Training at t=" + str(step*dt) + " with x(t)=" +
+                      str(batch_train[0].numpy()) + " and x(t+1)=" + str(batch_train[1].numpy()))
+                print("Prediction at this step: " + str(x_f.numpy()))
+                print("step %d: mean loss = %.4f" %
+                      (step, loss_metric.result()))
+             # Save Model
+            model.save(save_name)
 
-    test = model(test_x,t_0=t_0, t_f=t_f)
-    plt.plot(test_x, test.numpy(), '-.')
-    plt.plot(test_x, test_y, '--')
-    plt.show()
+    return 0
+
+    # extract model weights:
+    weights = [model.tr]
+
+
+def test_model(model, ic, time_data, t_f, n_time):
+    print(time_data.shape)
+
+    t = np.linspace(0, t_f, n_time)
+    results = model.predict(inputs=tf.constant(
+        ic, dtype=tf.float32),  t_0=0, t_f=t_f, n_time=n_time)
+
+    x_time_series = results.states.numpy()
+    print(x_time_series.shape)
+
+    plt.plot(t, x_time_series, '-.')
+    plt.plot(t, time_data, '--')
+    plt.savefig("result.png")
     return 0
 
 
