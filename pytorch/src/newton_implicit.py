b1dcd3bf23ef388a7e4fb7cc4b86a64009634331
Steffen <steffen.schotthoefer@kit.edu>
2022-06-13 12:57:46 -0400

    linear implicit layer is not a good design

--- pytorch/src/newton_implicit.py
+++ pytorch/src/newton_implicit.py
@@ -59,6 +59,17 @@ class TanhNewtonImplicitLayer(nn.Module):
         self.weight = nn.Parameter(torch.empty((out_features, out_features), dtype=torch.float))  # W^T
         self.bias = nn.Parameter(torch.empty(out_features, dtype=torch.float))
         self.activation = torch.tanh
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
+        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
+        # https://github.com/pytorch/pytorch/issues/57109
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            nn.init.uniform_(self.bias, -bound, bound)
 
     def forward(self, x):
         # Run Newton's method outside of the autograd framework
