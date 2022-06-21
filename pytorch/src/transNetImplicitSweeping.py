5fc74c1a6a8d77fb6f1aeef8136869c05b1e6583
Steffen <steffen.schotthoefer@kit.edu>
2022-06-21 19:08:01 -0400

    started proper transnet sweeping

--- pytorch/src/transNetImplicitSweeping.py
+++ pytorch/src/transNetImplicitSweeping.py
@@ -12,7 +12,9 @@ class TransNetSweeping(nn.Module):
         self.units = units
         self.epsilon = epsilon
         self.dt = dt
-        self.device=device
+        self.tol = 1e-4
+
+        self.device = device
         self.linearInput = LinearLayer(input_dim, units)
         self.block1 = TransNetLayerSweeping(units, units, epsilon=epsilon, dt=dt, device=device)
         self.block2 = TransNetLayerSweeping(units, units, epsilon=epsilon, dt=dt, device=device)
@@ -22,13 +24,46 @@ class TransNetSweeping(nn.Module):
         self.linearOutput = LinearLayer(units, output_dim)
 
     def forward(self, x):
-        z = self.linearInput(x)
-        z = torch.cat((z, self.block1.activation(z)), 1)
-        sweep = torch.zeros(self.units * 2)[None, :].to(self.device)
-        z, sweep = self.block1(z, sweep)
-        z, sweep = self.block2(z, sweep)
-        z, sweep = self.block3(z, sweep)
-        z, sweep = self.block4(z, sweep)
+        x = self.linearInput(x)
+        z_in = torch.cat((x, self.block1.activation(x)), 1)
+
+        # Source_Iteration
+        with torch.no_grad():
+            # 1) initialize model
+            self.block1.initialize_model(x)
+            self.block2.initialize_model(x)
+            self.block3.initialize_model(x)
+            self.block4.initialize_model(x)
+
+            # 2) Setup system matrix
+            self.block1.setup_system_mat()
+            self.block2.setup_system_mat()
+            self.block3.setup_system_mat()
+            self.block4.setup_system_mat()
+
+            step = 0
+            total_err = 10
+            while step < self.steps or total_err < self.tol:
+                # 1) relax
+                self.block1.relax()
+                self.block2.relax()
+                self.block3.relax()
+                self.block4.relax()
+
+                # 2) sweep
+                z, err1 = self.block1.sweep(z_in)
+                z, err2 = self.block2.sweep(z)
+                z, err3 = self.block3.sweep(z)
+                z, err4 = self.block4.sweep(z)
+                total_err = 1. / 4. * (err1 + err2 + err3 + err4)
+
+        # Forward iteration for gradient tape
+        z = self.block1.implicit_forward(z_in)
+        z = self.block2.implicit_forward(z)
+        z = self.block3.implicit_forward(z)
+        z = self.block4.implicit_forward(z)
+
+        #  linear output
         z = z[:, :self.units]
         z = self.linearOutput(z)
         logits = nn.Softmax(dim=1)(z)
@@ -62,7 +97,9 @@ class TransNetLayerSweeping(nn.Module):
     out_features: int
     weight: Tensor
 
-    # A:Tensor
+    A: Tensor
+    rhs: Tensor
+    z_k: Tensor
 
     def __init__(self, in_features: int, out_features: int, bias: bool = True, epsilon=0.01, dt=0.1, device="CPU",
                  dtype=None) -> None:
@@ -81,6 +118,7 @@ class TransNetLayerSweeping(nn.Module):
             self.register_parameter('bias', None)
         self.reset_parameters()
         self.activation = nn.Tanh()
+        self.A = torch.eye(2 * self.out_features).to(self.device)
 
     def reset_parameters(self) -> None:
         # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
@@ -121,8 +159,8 @@ class TransNetLayerSweeping(nn.Module):
 
             y = torch.solve(rhs, A)[0][:, :, 0]
 
-            w = self.weight.clone().detach().requires_grad_(False) # copy weights for forward pass, such that only the parameters of the first relaxation equation is used for gradient updates
-
+            w = self.weight.clone().detach().requires_grad_(
+                False)  # copy weights for forward pass, such that only the parameters of the first relaxation equation is used for gradient updates
 
         # 3)  reengage autograd and add the gradient hook
         u = y[:, :self.out_features]
@@ -130,8 +168,9 @@ class TransNetLayerSweeping(nn.Module):
         # a) u part
         u_out = self.dt * torch.matmul(v, self.weight.T) - u_in - self.dt * self.bias
         # b) v part
-        v_out = - self.dt * torch.matmul(u, w) +  self.dt / self.epsilon * v - v_in - self.dt / self.epsilon * self.activation(u_in)
-
+        v_out = - self.dt * torch.matmul(u,
+                                         w) + self.dt / self.epsilon * v - v_in - self.dt / self.epsilon * self.activation(
+            u_in)
 
         # Assemble layer solution vector
         y = torch.cat((u_out, v_out), 1)
@@ -145,3 +184,58 @@ class TransNetLayerSweeping(nn.Module):
         if y.requires_grad:
             y.register_hook(lambda grad: torch.solve(grad[:, :, None], A.transpose(1, 2))[0][:, :, 0])
         return y, sweep
+
+    def initialize_model(self, input_x):
+        """
+        :param input_x: input data
+        :return:
+        """
+        self.z_l = torch.cat((input_x, self.activation(input_x)), 1)
+
+        return self.z_l
+
+    def setup_system_mat(self):
+        """
+        :param z_in: [u,v]
+        :return: constructs the current System matrix.
+                DONT USE WITH GRADIENT TAPE ACTIVE
+        """
+        # 1)  assemble system matrix
+        self.A[:self.out_features, :self.out_features] = torch.eye(self.out_features, device=self.device)
+        self.A[:self.out_features, self.out_features:] = self.dt * self.weight
+        self.A[self.out_features:, :self.out_features] = - self.dt * torch.transpose(self.weight, 0, 1)
+        self.A[self.out_features:, self.out_features:] = (1 + self.dt / self.epsilon) * torch.eye(self.out_features,
+                                                                                                  device=self.device)
+        return 0
+
+    def relax(self):
+        """
+        :param z_in: [u,v]
+        :return: constructs the rhs of the relaxation system.
+                DONT USE WITH GRADIENT TAPE ACTIVE
+        """
+        # 1)  assemble right hand side
+        self.rhs = torch.cat(
+            (self.dt * self.bias, self.dt / self.epsilon * self.activation(self.z_l[:, :self.out_features])), 1)
+
+        return 0
+
+    def sweep(self, z_lp1_i):
+        """
+            :param z_lp1_i: sweep solve of previous layer
+            :return: Output y of implicit Layer Ay = x + f(x) + b
+                    DONT USE WITH GRADIENT TAPE ACTIVE
+        """
+        A = self.A.repeat(self.z_l.shape[0], 1, 1).to(self.device)
+        rhs = self.rhs + z_lp1_i
+
+        rhs = rhs[:, :, None]
+
+        y = torch.solve(rhs, A)[0][:, :, 0]
+        error = torch.mean(torch.linalg.norm(self.z_l - y, dim=1))
+        self.z_l = y
+        return self.z_l, error
+
+    def implicit_forward(self, z_in):
+
+        return z_out
